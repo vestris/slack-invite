04d6ebed92ef09bf109252c627fd50bd4b7176c6
dblock <dblock@dblock.org>
2021-02-26 10:45:39 -0500

    Replaced deprecated slack methods.

--- slack-invite/models/team.rb
+++ slack-invite/models/team.rb
@@ -39,9 +39,8 @@ class Team
   end
 
   def slack_channels
-    slack_client.channels_list(
-      exclude_archived: true,
-      exclude_members: true
+    slack_client.conversations_list(
+      exclude_archived: true
     )['channels'].select do |channel|
       channel['is_member']
     end
@@ -64,7 +63,7 @@ class Team
   # returns DM channel
   def inform_admin!(message)
     return unless activated_user_id
-    channel = slack_client.im_open(user: activated_user_id)
+    channel = slack_client.conversations_open(users: activated_user_id.to_s)
     message_with_channel = message.merge(channel: channel.channel.id, as_user: true)
     logger.info "Sending DM '#{message_with_channel.to_json}' to #{activated_user_id}."
     rc = slack_client.chat_postMessage(message_with_channel)
@@ -182,7 +181,7 @@ EOS
     return unless active? && activated_user_id && bot_user_id
     return unless active_changed? || activated_user_id_changed?
 
-    im = slack_client.im_open(user: activated_user_id)
+    im = slack_client.conversations_open(users: activated_user_id.to_s)
     slack_client.chat_postMessage(
       text: activated_text,
       channel: im['channel']['id'],
